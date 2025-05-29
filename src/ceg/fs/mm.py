589be45e05f91e283d958e76b763be70a8c48fe3
tomjrwilliams <tomrjw@gmail.com>
2025-05-29 20:22:00 +0800

    comment out mm until i can refactor that as well

--- src/ceg/fs/mm.py
+++ src/ceg/fs/mm.py
@@ -1,89 +1,96 @@
-from typing import NamedTuple, ClassVar
-import numpy
-import numpy as np
+# from typing import NamedTuple, ClassVar
+# import numpy
+# import numpy as np
 
-from ..core import (
-    Graph,
-    Node,
-    Ref,
-    Event,
-    Loop,
-    Defn,
-    define,
-    steps,
-)
-
-#  ------------------
-
-# TODO: vs_x_mat to do more than one factor decomp (below is strictly oen)
+# from ..core import (
+#     Graph,
+#     Node,
+#     Ref,
+#     Event,
+#     Loop,
+#     Defn,
+#     define,
+#     steps,
+# )
 
 
-class vs_x_vec_kw(NamedTuple):
-    type: str
-    #
-    vs: tuple[Ref.D0_F64, ...]
-    vec: Ref.D1_F64
+# #  ------------------
 
 
-class vs_x_vec(vs_x_vec_kw, Node.D0_F64):
-
-    DEF: ClassVar[Defn] = define(Node.D0_F64, vs_x_vec_kw)
-
-    @classmethod
-    def new(
-        cls,
-        vs: tuple[Ref.D0_F64, ...],
-        vec: Ref.D1_F64,
-    ):
-        return cls(cls.DEF.name, vs=vs, vec=vec)
-
-    def __call__(self, event: Event, graph: Graph):
-        vs = list(
-            map(
-                lambda v: v.history(graph).last_before(
-                    event.t
-                ),
-                self.vs,
-            )
-        )
-        vec = self.vec.history(graph).last_before(event.t)
-        vs = np.array(vs)
-        all_null = np.all(np.isnan(vs))
-        if all_null:
-            return np.NAN
-        return np.dot(vs, vec)
+# TODO: still need to rewrite to the new history implementation....
 
 
-#  ------------------
+# #  ------------------
+
+# # TODO: vs_x_mat to do more than one factor decomp (below is strictly oen)
 
 
-class v_x_vec_i_kw(NamedTuple):
-    type: str
-    #
-    v: Ref.D0_F64
-    vec: Ref.D1_F64
-    i: int
+# class vs_x_vec_kw(NamedTuple):
+#     type: str
+#     #
+#     vs: tuple[Ref.D0_F64, ...]
+#     vec: Ref.D1_F64
 
 
-class v_x_vec_i(v_x_vec_i_kw, Node.D0_F64):
+# class vs_x_vec(vs_x_vec_kw, Node.D0_F64):
 
-    DEF: ClassVar[Defn] = define(Node.D0_F64, v_x_vec_i_kw)
+#     DEF: ClassVar[Defn] = define(Node.D0_F64, vs_x_vec_kw)
 
-    @classmethod
-    def new(
-        cls,
-        v: Ref.D0_F64,
-        vec: Ref.D1_F64,
-        i: int,
-    ):
-        return cls(cls.DEF.name, v=v, vec=vec, i=i)
+#     @classmethod
+#     def new(
+#         cls,
+#         vs: tuple[Ref.D0_F64, ...],
+#         vec: Ref.D1_F64,
+#     ):
+#         return cls(cls.DEF.name, vs=vs, vec=vec)
 
-    def __call__(self, event: Event, graph: Graph):
-        v = self.v.history(graph).last_before(event.t)
-        vec = self.vec.history(graph).last_before(event.t)
-        all_null = np.all(np.isnan(vec))
-        if all_null:
-            return np.NAN
-        if np.isnan(v):
-            return np.NAN
-        return vec[self.i] * v
+#     def __call__(self, event: Event, graph: Graph):
+#         vs = list(
+#             map(
+#                 lambda v: v.history(graph).last_before(
+#                     event.t
+#                 ),
+#                 self.vs,
+#             )
+#         )
+#         vec = self.vec.history(graph).last_before(event.t)
+#         vs = np.array(vs)
+#         all_null = np.all(np.isnan(vs))
+#         if all_null:
+#             return np.NAN
+#         return np.dot(vs, vec)
+
+
+# #  ------------------
+
+
+# class v_x_vec_i_kw(NamedTuple):
+#     type: str
+#     #
+#     v: Ref.D0_F64
+#     vec: Ref.D1_F64
+#     i: int
+
+
+# class v_x_vec_i(v_x_vec_i_kw, Node.D0_F64):
+
+#     DEF: ClassVar[Defn] = define(Node.D0_F64, v_x_vec_i_kw)
+
+#     @classmethod
+#     def new(
+#         cls,
+#         v: Ref.D0_F64,
+#         vec: Ref.D1_F64,
+#         i: int,
+#     ):
+#         return cls(cls.DEF.name, v=v, vec=vec, i=i)
+
+#     def __call__(self, event: Event, graph: Graph):
+#         v = self.v.history(graph).last_before(event.t)
+#         vec = self.vec.history(graph).last_before(event.t)
+#         all_null = np.all(np.isnan(vec))
+#         if all_null:
+#             return np.NAN
+#         if np.isnan(v):
+#             return np.NAN
+#         return vec[self.i] * v
